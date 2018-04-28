a83b5bebd671e518456f461db33827e085e487cb
nonoka <1160199051@zust.edu.cn>
2018-04-28 21:55:32 +0800

    Mooc

---
+++ 1.1 完美立方.py
@@ -0,0 +1,7 @@
+N = int(input())
+for a in range(2,N+1):
+    for b in range(2,a):
+        for c in range(b,a):
+            for d in range(c,a):
+                if a**3 == b**3 + c**3 + d**3:
+                    print("Cube = %d,Triple = (%d,%d,%d)"%(a,b,c,d))
